be034895370c6b387e7d266aa0eaa4d1a5e4d46d
luis.pires <luispiresqa@gmail.com>
2020-09-12 21:25:47 -0300

    Criando o initialize() para colocar os valores dentro do display da calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,36 @@
 class CalcController{
 
     constructor(){
-        
+
+        let _displayCalEl = document.querySelector("#display")
+        let _dateEl =  document.querySelector("#data")
+         let _timeEl = document.querySelector("#hora")
+        this._displayCalc = "0"
+        this._currentDate
+        this.initialize()
     }
 
+    initialize(){
+        displayCalEl.innerHTML = "4567"
+        dateEl.innerHTML = new Date().toLocaleDateString('pt-BR')
+        timeEl.innerHTML = "00:00"
+    }
+
+    get displaycalc(){
+
+        return this._displayCalc
+    }
+
+    set displaycalc(valor){
+        this._displayCalc = valor
+    }
+
+    get currentDate(){
+
+       return this._currentDate
+    }
+
+    set currentDate(valor ){
+        this._currentDate=valor
+    }
 }
