b6031982b491e5eb7bc6319e3db6c972fadc041f
luis.pires <luispiresqa@gmail.com>
2020-09-13 21:32:01 -0300

    Adicionando eventos de mouseup, mousedown e mouseover com o pointer como mouse

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -6,8 +6,11 @@ class CalcController {
         this._displayCalcEl = document.querySelector("#display")
         this._dateEl = document.querySelector("#data")
         this._timeEl = document.querySelector("#hora")
-        this._currentDate
+        this._currentDate   
+    
+        this.initButtonsEvents()  
         this.initialize()
+        
     }
 
     initialize() {
@@ -22,10 +25,40 @@ class CalcController {
 
     }
 
+
+    addEventListenerAll(element, events, fn){
+
+        
+        events.split(' ').forEach(event =>{
+                    
+         
+                     element.addEventListener(event, fn, false)
+                 })
+         
+             }
+
     initButtonsEvents(){
 
-        document.querySelectorAll("#buttons > g, #parts > g")
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+
+        buttons.forEach((btn, index) =>{
+
+            this.addEventListenerAll(btn,"click drag", e => {
+
+                console.log(btn.className.baseVal.replace("btn-",""))
+            })
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer"
+            })
+        })
     }
+
+
+
+   
 
     setDisplayDateTime() {
 
