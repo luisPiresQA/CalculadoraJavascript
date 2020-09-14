a2afed2838599a2e1d49b08608eee4c297a9506a
luis.pires <luispiresqa@gmail.com>
2020-09-13 22:07:39 -0300

    Criando as funcionalidades dos botões clear e validado a funcionalidade do push()

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,22 +2,23 @@ class CalcController {
 
     constructor() {
 
+        this._operation = []
         this._locale = 'pt-BR'
         this._displayCalcEl = document.querySelector("#display")
         this._dateEl = document.querySelector("#data")
         this._timeEl = document.querySelector("#hora")
-        this._currentDate   
-    
-        this.initButtonsEvents()  
+        this._currentDate
+
+        this.initButtonsEvents()
         this.initialize()
-        
+
     }
 
     initialize() {
 
         this.setDisplayDateTime()
 
-       setInterval(() => {
+        setInterval(() => {
 
             this.setDisplayDateTime()
 
@@ -26,27 +27,102 @@ class CalcController {
     }
 
 
-    addEventListenerAll(element, events, fn){
+    addEventListenerAll(element, events, fn) {
 
-        
-        events.split(' ').forEach(event =>{
-                    
-         
-                     element.addEventListener(event, fn, false)
-                 })
-         
-             }
 
-    initButtonsEvents(){
+        events.split(' ').forEach(event => {
+
+
+            element.addEventListener(event, fn, false)
+        })
+
+    }
+
+    clearAll(){
+            this._operation = []
+    }
+
+    clearEntry(){
+
+        this._operation.pop()
+    }
+
+    addOperation(value){
+
+        this._operation.push(value)
+
+        console.log( this._operation)
+    }
+
+    setError(){
+
+        this.displayCalc = "Error"
+    }
+
+    execBtn(value){
+
+        switch(value){
+
+            case 'ac':
+                this.clearAll()
+                break
+                
+            case 'ce':
+                this.clearAll()
+                break
+                
+            case 'soma':
+                this.clearAll()
+                break
+                
+            case 'divisao':
+                this.clearAll()
+                break
+                
+            case 'multiplicacao':
+                this.clearAll()
+                break
+
+                case 'porcento':
+                this.clearAll()
+                break
+
+                case 'igual':
+                this.clearAll()
+                break
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(value))
+                break
+
+                default :
+                    this.setError()
+                    break
+        }
+
+    }
+
+    initButtonsEvents() {
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g")
 
 
-        buttons.forEach((btn, index) =>{
+        buttons.forEach((btn, index) => {
 
-            this.addEventListenerAll(btn,"click drag", e => {
+            this.addEventListenerAll(btn, "click drag", e => {
 
-                console.log(btn.className.baseVal.replace("btn-",""))
+                let textBtn = btn.className.baseVal.replace("btn-", "")
+
+                this.execBtn(textBtn)
             })
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
@@ -56,13 +132,9 @@ class CalcController {
         })
     }
 
-
-
-   
-
     setDisplayDateTime() {
 
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: '2-digit',
             month: 'long',
             year: 'numeric'
